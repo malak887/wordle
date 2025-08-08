3da38b89b671edc884f1d1135b7318b6461c2dc2
malak887 <malakawny0808@gmail.com>
2025-08-08 20:39:13 +0300

    implementation of game

---
+++ main.py
@@ -0,0 +1,37 @@
+
+from encrypted_word import *
+from check import*
+from colour import*
+encrypted=get_word()
+
+print("quess the word and win !!")
+print("you have 6 attempts")
+
+flag=False
+for k in range(6):
+    attempt = input()
+    if attempt==encrypted:flag=True
+    l_right=right_pos(attempt,encrypted)
+    l_found= letter_found(attempt,encrypted)
+    output=""
+    for x in range(5):
+        if l_right[x]==1: 
+         output= colouring(attempt[x],output,"green")
+        elif l_found[x]==1:
+         output=colouring(attempt[x],output,"yellow")
+        else: output +=attempt[x] 
+    print("               ",output)  
+    if flag :
+       print("correct guess")
+       print("you won")
+       break
+    
+   
+   
+        
+    
+
+
+
+
+ 
