31fc332fe171029b3539325a5ce4192b669a6fd3
malak887 <malakawny0808@gmail.com>
2025-08-08 22:25:47 +0300

    solve a bug

--- check.py
+++ check.py
@@ -1,5 +1,7 @@
-l_right=[0,0,0,0,0]
+
 def right_pos(word,encrypted):#if the letter is right and right posistion , will have a true in list
+    
+    l_right = [0,0,0,0,0]
     for i in range(5):
         if word[i]==encrypted[i]:
             l_right[i]=1
@@ -7,10 +9,11 @@ def right_pos(word,encrypted):#if the letter is right and right posistion , will
 
 
 
-l_found=[0,0,0,0,0]
+
 def letter_found(word,encrypted):#if the letter is right and wrong posistion , will have a true in list
+    l_found=[0,0,0,0,0]
     for i in range(5):
         x= encrypted.find(word[i])
-        if x>-1  and l_right[i]==0:
+        if x>-1 :
             l_found[i]=1
     return l_found
