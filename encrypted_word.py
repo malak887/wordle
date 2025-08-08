b5eee703591c2274ee3ba0c61722a455b8633edf
malak887 <malakawny0808@gmail.com>
2025-08-08 20:01:03 +0300

    modify function name

--- encrypted_word.py
+++ encrypted_word.py
@@ -2,7 +2,6 @@ import random
 with open("words.txt", "r") as f:
     data = [line.strip() for line in f] #read the file , and save the strings in a list
 
-def encrypted_word():
+def get_word():
   
     return random.choice(data) #choose a random string from the list
-print(encrypted_word())
